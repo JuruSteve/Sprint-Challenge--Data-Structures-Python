88804d2c9a0ede1c65c51d28c07a617a79c2db9c
jurusteve <jstevemu17@gmail.com>
2019-10-18 13:58:39 -0400

    done

--- names/names.py
+++ names/names.py
@@ -1,5 +1,51 @@
 import time
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
+    # Insert the given value into the tree
+    # check if new node's val value < curr node val self.value
+    # check if not self.left , self.left = BinarySearchTree(value)
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                return self.left.insert(value)
+
+    # check if new node's val value < curr node val self.value
+    # check if not self.left , self.left = BinarySearchTree(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                return self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if not self.right:
+            return self.value
+        return self.right.get_max()
+        
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
