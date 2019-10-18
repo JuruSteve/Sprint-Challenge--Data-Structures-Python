b8d11a96c4d148c074b4a45b7a4cdbc6735cdaa2
jurusteve <jstevemu17@gmail.com>
2019-10-18 17:28:22 -0400

    modified names.py

--- names/names.py
+++ names/names.py
@@ -1,51 +1,4 @@
 import time
-class BinarySearchTree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-    # Insert the given value into the tree
-    # check if new node's val value < curr node val self.value
-    # check if not self.left , self.left = BinarySearchTree(value)
-
-    def insert(self, value):
-        if value < self.value:
-            if not self.left:
-                self.left = BinarySearchTree(value)
-            else:
-                return self.left.insert(value)
-
-    # check if new node's val value < curr node val self.value
-    # check if not self.left , self.left = BinarySearchTree(value)
-        else:
-            if not self.right:
-                self.right = BinarySearchTree(value)
-            else:
-                return self.right.insert(value)
-
-    # Return True if the tree contains the value
-    # False if it does not
-    def contains(self, target):
-        if self.value == target:
-            return True
-        if target < self.value:
-            if not self.left:
-                return False
-            else:
-                return self.left.contains(target)
-        else:
-            if not self.right:
-                return False
-            else:
-                return self.right.contains(target)
-
-    # Return the maximum value found in the tree
-    def get_max(self):
-        if not self.right:
-            return self.value
-        return self.right.get_max()
-        
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -58,11 +11,9 @@ f.close()
 
 duplicates = []
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
+    if name_1 in names_2:
             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
